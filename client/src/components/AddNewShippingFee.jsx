4b3ac9646042614b72802da41b4d27e237b64819
Nguyễn Tuấn Kiệt <kiet5454aa@gmail.com>
2024-10-16 17:46:43 +0700

    first commit

--- client/src/components/AddNewShippingFee.jsx
+++ client/src/components/AddNewShippingFee.jsx
@@ -54,47 +54,46 @@ function AddNewShippingFee({ onClose }) {
 
     dispatch(setShippingFee(selectedCity, selectedDistrict, shippingFeeValue));
     onClose();
+  };
 
-    return (
-      <Box>
-        {/* City Dropdown */}
-        <Select onChange={handleCityChange} value={selectedCity}>
-          <option value="">Chọn tình / thành phố</option>
-          {cities.map((city) => (
-            <option key={city.province_id} value={city.province_name}>
-              {city.province_name}
+  return (
+    <Box>
+      <Select onChange={handleCityChange} value={selectedCity}>
+        <option value="">Chọn tình / thành phố</option>
+        {cities.map((city) => (
+          <option key={city.province_id} value={city.province_name}>
+            {city.province_name}
+          </option>
+        ))}
+      </Select>
+
+      {districts.length > 0 && (
+        <Select
+          onChange={(e) => setSelectedDistrict(e.target.value)}
+          value={selectedDistrict}
+          mt={2}
+        >
+          <option value="">Chọn quận huyện (nếu cần)</option>
+          {districts.map((district) => (
+            <option key={district.district_id} value={district.district_name}>
+              {district.district_name}
             </option>
           ))}
         </Select>
+      )}
 
-        {districts.length > 0 && (
-          <Select
-            onChange={(e) => setSelectedDistrict(e.target.value)}
-            value={selectedDistrict}
-            mt={2}
-          >
-            <option value="">Chọn quận huyện (nếu cần)</option>
-            {districts.map((district) => (
-              <option key={district.district_id} value={district.district_name}>
-                {district.district_name}
-              </option>
-            ))}
-          </Select>
-        )}
+      <Input
+        mt={2}
+        type="number"
+        placeholder="Nhập giá ship"
+        value={shippingFeeValue}
+        onChange={(e) => setShippingFeeValue(e.target.value)}
+      />
 
-        <Input
-          mt={2}
-          type="number"
-          placeholder="Nhập giá ship"
-          value={shippingFeeValue}
-          onChange={(e) => setShippingFeeValue(e.target.value)}
-        />
-
-        <Button mt={4} colorScheme="teal" onClick={handleSubmit}>
-          Add Shipping Fee
-        </Button>
-      </Box>
-    );
-  };
+      <Button mt={4} colorScheme="teal" onClick={handleSubmit}>
+        Add Shipping Fee
+      </Button>
+    </Box>
+  );
 }
 export default AddNewShippingFee;
